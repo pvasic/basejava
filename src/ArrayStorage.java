5565b6bd51d07c4c2895b4fbbba549606c65be06
Pavel Vasichkin <pavasichkin@yandex.ru>
2019-11-17 22:53:14 +0300

    Fix the HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,44 +6,46 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    static final int MASS_SIZE = 10000;
+    Resume[] storage;
+    int rsize;
 
     public ArrayStorage() {
-        Arrays.stream(storage).forEach(resume -> resume = null);
+        this.storage = new Resume[MASS_SIZE];
+        this.rsize = 0;
     }
 
     void clear() {
-        for (int i = 0; storage[i] != null; i++) {
+        for (int i = 0; i < rsize; i++) {
             storage[i] = null;
         }
+        rsize = 0;
     }
 
     void save(Resume r) {
-
         boolean equals = false;
         int i = 0;
-        for (i = 0; storage[i] != null; i++) {
+        for (i = 0; i < rsize; i++) {
             if (storage[i].uuid.equals(r.uuid)) {
                 equals = true;
             }
         }
-        if (equals == false && i >= 0) {
-            try {
-                storage[i] = r;
-            } catch (ArrayIndexOutOfBoundsException e) {
-                e.printStackTrace();
-            }
-
+        if (equals == false && i < MASS_SIZE) {
+            storage[i] = r;
+            rsize++;
         } else {
             if (equals == true)
-                System.out.println("Такое резюме как: " + r.uuid + " уже существует");
+                System.out.println("Такое резюме как: " + r.uuid + " уже существует!"); // Вне main() использовать log4j
+            else {
+                if (equals == false && i >= MASS_SIZE)
+                    System.out.println("База резюме заполнена! Освободите память."); // Вне main() использовать log4j
+            }
         }
     }
 
     Resume get(String uuid) {
-
         Resume resume = null;
-        for (int i = 0; storage[i] != null; i++) {
+        for (int i = 0; i < rsize; i++) {
             if (storage[i].uuid.equals(uuid))
                 resume = storage[i];
         }
@@ -51,38 +53,32 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-
         boolean equals = false;
         int del = 0;
         int i;
-        for (i = 0; storage[i] != null; i++) {
+        for (i = 0; i < rsize; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 del = i;
                 equals = true;
             }
         }
         if (equals == false) {
-            System.out.println("Удаление не возможно, такое резюме как: " + uuid + " не найдено!");
+            System.out.println("Удаление не возможно, такое резюме как: " + uuid + " не найдено!"); // Вне main() использовать log4j
         } else {
-            storage[del] = storage[i-1];
-            storage[i-1] = null;
+            storage[del] = storage[i - 1];
+            storage[i - 1] = null;
+            rsize--;
         }
-
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     Resume[] getAll() {
-        Resume[] resumes = new Resume[size()];
-        for (int i = 0; i < resumes.length; i++) resumes[i] = storage[i];
+        Resume[] resumes = new Resume[rsize];
+        for (int i = 0; i < rsize; i++)
+            resumes[i] = storage[i];
         return resumes;
     }
 
     int size() {
-        int i = 0;
-        for (i = 0; storage[i] != null; i++) {
-        }
-        return i;
+        return rsize;
     }
 }
