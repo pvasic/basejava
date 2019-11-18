6e433e4ec711a7326774f04697ab392204bf9587
Pavel Vasichkin <pavasichkin@yandex.ru>
2019-11-18 15:38:51 +0300

    HW1. Optimizes method save(), delete(). Ver-1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,84 +1,84 @@
-import org.omg.CosNaming.NamingContextPackage.NotFound;
-
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    static final int MASS_SIZE = 10000;
+    static final int ARRAY_SIZE = 10000;
     Resume[] storage;
-    int rsize;
+    int size;
 
     public ArrayStorage() {
-        this.storage = new Resume[MASS_SIZE];
-        this.rsize = 0;
+        this.storage = new Resume[ARRAY_SIZE];
+        this.size = 0;
     }
 
     void clear() {
-        for (int i = 0; i < rsize; i++) {
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
-        rsize = 0;
+        size = 0;
     }
 
-    void save(Resume r) {
-        boolean equals = false;
-        int i = 0;
-        for (i = 0; i < rsize; i++) {
-            if (storage[i].uuid.equals(r.uuid)) {
-                equals = true;
-            }
+    void save(Resume resume) {
+        if (size == ARRAY_SIZE) {
+            System.out.println("База резюме заполнена! Освободите память."); // Вне main() использовать log4j
+            return;
         }
-        if (equals == false && i < MASS_SIZE) {
-            storage[i] = r;
-            rsize++;
+        if (size == 0) {
+            storage[size] = resume;
+            size++;
         } else {
-            if (equals == true)
-                System.out.println("Такое резюме как: " + r.uuid + " уже существует!"); // Вне main() использовать log4j
-            else {
-                if (equals == false && i >= MASS_SIZE)
-                    System.out.println("База резюме заполнена! Освободите память."); // Вне main() использовать log4j
+            for (int i=0; i < size; i++) {
+                if (storage[i].uuid.equals(resume.uuid)) {
+                    System.out.println("Такое резюме как: " + resume.uuid + " уже существует!"); // Вне main() использовать log4j
+                    return;
+                } else {
+                    if (i == size - 1) {
+                        storage[size] = resume; // Избегать дублирования кода
+                        size++;
+                    }
+                }
             }
         }
     }
 
     Resume get(String uuid) {
         Resume resume = null;
-        for (int i = 0; i < rsize; i++) {
-            if (storage[i].uuid.equals(uuid))
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
                 resume = storage[i];
+            }
         }
         return resume;
     }
 
     void delete(String uuid) {
-        boolean equals = false;
-        int del = 0;
-        int i;
-        for (i = 0; i < rsize; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                del = i;
-                equals = true;
-            }
-        }
-        if (equals == false) {
-            System.out.println("Удаление не возможно, такое резюме как: " + uuid + " не найдено!"); // Вне main() использовать log4j
+        if (size == 0) {
+            return;
         } else {
-            storage[del] = storage[i - 1];
-            storage[i - 1] = null;
-            rsize--;
+            int  i = 0;
+            for (; i < size; i++) {
+                if (storage[i].uuid.equals(uuid)) {
+                    storage[i]=storage[size-1];
+                    storage[size-1]=null;
+                    size--;
+                    return;
+                }
+            }
+            if (i == size) {
+                System.out.println("Удаление не возможно, такое резюме как: " + uuid + " не найдено!"); // Вне main() использовать log4j
+            }
         }
     }
 
     Resume[] getAll() {
-        Resume[] resumes = new Resume[rsize];
-        for (int i = 0; i < rsize; i++)
+        Resume[] resumes = new Resume[size];
+        for (int i = 0; i < size; i++) {
             resumes[i] = storage[i];
+        }
         return resumes;
     }
 
     int size() {
-        return rsize;
+        return size;
     }
 }
