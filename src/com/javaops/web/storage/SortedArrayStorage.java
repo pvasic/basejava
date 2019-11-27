6513dc4337dd7500526dfc27fb391760b61fd863
pavasic <pl.vasichkin@gmail.com>
2019-11-27 23:37:46 +0300

    HW3 introduse abstract methods saveAndMove(), deleteAndMove(), getIndex()

--- src/com/javaops/web/storage/SortedArrayStorage.java
+++ src/com/javaops/web/storage/SortedArrayStorage.java
@@ -1,4 +1,41 @@
 package com.javaops.web.storage;
 
-public class SortedArrayStorage {
+import com.javaops.web.model.Resume;
+
+import java.util.Arrays;
+
+public class SortedArrayStorage extends AbstractArrayStorage {
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void update(Resume r) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    protected void saveAndMove(Resume resume) {
+
+    }
+
+    @Override
+    protected void deleteAndMove(int index) {
+        for (;index < size;index++) {
+            storage[index] = storage[index+1];
+        }
+    }
+
+    @Override
+    protected int getIndex(String uuid) {
+        Resume searchKey = new Resume();
+        searchKey.setUuid(uuid);
+        return Arrays.binarySearch(storage, 0, size, searchKey);
+    }
 }
