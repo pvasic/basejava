2701368a51590f1ce44f04f91d21beace915d6fe
pavasic <pl.vasichkin@gmail.com>
2019-12-20 12:12:42 +0300

    HW05 introduce MapStorage.java

---
+++ src/com/javaops/web/storage/MapStorage.java
@@ -0,0 +1,62 @@
+package com.javaops.web.storage;
+
+import com.javaops.web.model.Resume;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Vasichkin Pavel
+ */
+public class MapStorage extends AbstractStorage {
+    private static final Map<String, Resume> storage = new HashMap<>();
+
+    @Override
+    protected Object searchKey(String uuid) {
+        if (storage.containsKey(uuid)) {
+            return uuid;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    protected boolean isKey(Object key) {
+        return key != null;
+    }
+
+    @Override
+    protected Resume getResume(Object key) {
+        return storage.get(key);
+    }
+
+    @Override
+    protected void saveResume(Resume resume, Object key) {
+        storage.put((String) key, resume);
+    }
+
+    @Override
+    protected void updateResume(Object key, Resume resume) {
+        storage.put((String) key, resume);
+    }
+
+    @Override
+    protected void deleteResume(Object key) {
+        storage.remove(key);
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return storage.values().toArray(new Resume[0]);
+    }
+}
