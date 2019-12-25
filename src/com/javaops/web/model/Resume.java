3751edf428e07443f4b663e78ae14e0e3359d1f9
pavasic <pl.vasichkin@gmail.com>
2019-12-25 20:58:34 +0300

    HW06 fix Resume.java

--- src/com/javaops/web/model/Resume.java
+++ src/com/javaops/web/model/Resume.java
@@ -45,24 +45,29 @@ public class Resume implements Comparable<Resume> {
         if (o == null || getClass() != o.getClass())
             return false;
         Resume resume = (Resume) o;
-        return Objects.equals(getUuid(), resume.getUuid()) && Objects.equals(getFullName(), resume.getFullName());
+        return Objects.equals(uuid, resume.uuid) && Objects.equals(fullName, resume.fullName);
     }
 
     @Override
     public int hashCode() {
         int result = 17;
-        result = 37 * result + ((getUuid() == null) ? 0 : getUuid().hashCode());
-        result = 37 * result + ((getFullName() == null) ? 0 : getFullName().hashCode());
+        result = 37 * result + ((uuid == null) ? 0 : uuid.hashCode());
+        result = 37 * result + ((fullName == null) ? 0 : fullName.hashCode());
         return result;
     }
 
     @Override
     public String toString() {
-        return getUuid();
+        return uuid;
     }
 
     @Override
     public int compareTo(Resume resume) {
-        return getFullName().compareTo(resume.getFullName());
+        int result = fullName.compareTo(resume.fullName);
+        if (result == 0) {
+            return uuid.compareTo(uuid);
+        } else {
+            return result;
+        }
     }
 }
