99d2a3ca0e96abede9698f1b39b57065663212e3
pavasic <pl.vasichkin@gmail.com>
2020-01-28 23:26:33 +0300

    HW08 update MainFile.java walkFileTree

--- src/com/javaops/web/run/MainFile.java
+++ src/com/javaops/web/run/MainFile.java
@@ -3,12 +3,16 @@ package com.javaops.web.run;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
 
 /**
  * @author Vasichkin Pavel
  */
 public class MainFile {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String filePath = ".//.gitignore";
 
         File file = new File(filePath);
@@ -31,6 +35,56 @@ public class MainFile {
             System.out.println(fis.read());
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+
+        Path directory = Paths.get("./");
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(directory)) {
+            for (Path f: stream) {
+                System.out.println(f.getFileName());
+            }
+        } catch (IOException | DirectoryIteratorException x) {
+            throw new RuntimeException(x);
+        }
+
+        Files.walkFileTree(directory, new PrintFiles());
+    }
+
+    public static class PrintFiles
+            extends SimpleFileVisitor<Path> {
+
+        // Print information about
+        // each type of file.
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attr) {
+            if (attr.isSymbolicLink()) {
+                System.out.format("Symbolic link: %s ", file);
+            } else if (attr.isRegularFile()) {
+                System.out.format("Regular file: %s ", file);
+            } else {
+                System.out.format("Other: %s ", file);
+            }
+            System.out.println("(" + attr.size() + "bytes)");
+            return CONTINUE;
+        }
+
+        // Print each directory visited.
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir,
+                                                  IOException exc) {
+            System.out.format("Directory: %s%n", dir);
+            return CONTINUE;
+        }
+
+        // If there is some error accessing
+        // the file, let the user know.
+        // If you don't override this method
+        // and an error occurs, an IOException
+        // is thrown.
+        @Override
+        public FileVisitResult visitFileFailed(Path file,
+                                               IOException exc) {
+            System.err.println(exc);
+            return CONTINUE;
         }
     }
 }
